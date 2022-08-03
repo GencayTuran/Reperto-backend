f0a2e426127b57e4be85a12bc4b586fc6e604f3b
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-08-03 17:32:34 +0200

    added DbContext with tables

---
+++ Reperto/Reperto/Data/RepertoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reperto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reperto.Data
+{
+    public class RepertoDbContext : DbContext
+    {
+        public RepertoDbContext(DbContextOptions<RepertoDbContext>
+        options) : base(options) { }
+        
+        //Db tables
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Chord> Chords { get; set; }
+        public DbSet<Repertoire> Repertoires { get; set; }
+    }
+}
