6f21b09f917a741213ba467474edbaeeefe0d64f
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-08-03 17:32:45 +0200

    added migration

---
+++ Reperto/Reperto/Migrations/20220803133953_initial.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Reperto.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chords",
+                columns: table => new
+                {
+                    ChordId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chords", x => x.ChordId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Band = table.Column<string>(nullable: true),
+                    Lyrics = table.Column<string>(nullable: true),
+                    Mood = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Repertoires",
+                columns: table => new
+                {
+                    RepertoireId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SongId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repertoires", x => x.RepertoireId);
+                    table.ForeignKey(
+                        name: "FK_Repertoires_Songs_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Songs",
+                        principalColumn: "SongId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repertoires_SongId",
+                table: "Repertoires",
+                column: "SongId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chords");
+
+            migrationBuilder.DropTable(
+                name: "Repertoires");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
