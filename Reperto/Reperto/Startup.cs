81d398a3273fe852af695517390c36ea5b6a39db
gencay.turan@student.pxl.be <gencay.turan@student.pxl.be>
2022-08-11 22:40:27 +0200

    added CORS service

--- Reperto/Reperto/Startup.cs
+++ Reperto/Reperto/Startup.cs
@@ -25,6 +25,15 @@ namespace Reperto
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    name: "AllowOrigin",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                    });
+            });
             services.AddControllersWithViews();
             services.AddDbContext<RepertoDbContext>(opts => {
                 opts.UseSqlServer(
@@ -35,6 +44,7 @@ namespace Reperto
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowOrigin");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
